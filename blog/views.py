af7f13bc7d22e255b95a51b3c93b04fd4f4f89d4
luckyxiang <luckyxiang503@163.com>
2019-05-06 22:49:12 +0800

    day3

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
+from .models import Post, Category, Tag
 # Create your views here.
 
-def post_list(request):
-    pass
+
+def post_list(request, category_id=None, tag_id=None):
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.status_normal)
+    else:
+        post_list = Post.objects.filter(status=Post.status_normal)
+        if category_id:
+            post_list = post_list.objects.filter(category_id=category_id)
+
+    return render(request, "blog/list.html", context={'post_list': post_list})
 
 
-def post_detail(request):
-    pass
+def post_detail(request, post_id=None):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    return render(request, 'blog/detail.html', context={'post': post})
