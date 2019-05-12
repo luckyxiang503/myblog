d30a28922f41ce803fc716a0e99fac5ff397a4dc
luckyxiang <luckyxiang503@163.com>
2019-05-12 14:34:53 +0800

    由function views改为class views

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,15 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, converters
 
-from blog.views import post_detail, post_list
+# from blog.views import post_detail, post_list
+from blog.views import IndexView, CategoryView, TagView, PostDetailView
 from config.views import links
 
 
 urlpatterns = [
-    path('', post_list, name='index'),
-    re_path(r'^category/(?P<category_id>\d+)/$', post_list, name='category_list'),
-    re_path(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag_list'),
-    re_path(r'^post/(?P<post_id>\d+).html$', post_detail, name='post_detail'),
+    # path('', post_list, name='index'),
+    # re_path(r'^category/(?P<category_id>\d+)/$', post_list, name='category_list'),
+    # re_path(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag_list'),
+    # re_path(r'^post/(?P<post_id>\d+).html$', post_detail, name='post_detail'),
+    # re_path(r'^links/$', links, name='links'),
+
+    # 使用class views
+    path('', IndexView.as_view(), name='index'),
+    re_path(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category_list'),
+    re_path(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag_list'),
+    re_path(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post_detail'),
     re_path(r'^links/$', links, name='links'),
 
     path('admin/', admin.site.urls),
